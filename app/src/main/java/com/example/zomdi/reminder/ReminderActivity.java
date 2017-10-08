ede2413fcf601a3c9a53ee898a3d521ac960bf99
zomdi <subczym@gmail.com>
2017-10-08 15:40:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zomdi/reminder/ReminderActivity.java
@@ -0,0 +1,14 @@
+package com.example.zomdi.reminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+
+    }
+}
